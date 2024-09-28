b37a9d34f27b694f97c74ea48bcd70cc436c018d
mujeebalish <mujeebrehman12122@gmail.com>
2024-09-28 02:45:15 -0700

    1

--- main.js
+++ main.js
@@ -40,11 +40,11 @@ document.getElementById('adminBtn').addEventListener('click', function() {
           
         });
         // onAut state change
-        onAuthStateChanged(auth, (user) => {
+          onAuthStateChanged(auth, (user) => {
           if (user) {
             // User is signed in, see docs for a list of available properties
             // https://firebase.google.com/docs/reference/js/auth.user
-            const uid = user.uid;
+          window.location.href = "./dashbord/dashbord.html"
             // ...
           } else {
             alert("Pleas fulfil all fields")
