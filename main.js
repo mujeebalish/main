b9f6b67bc8c5cb101ba38495d4fce24e71ff8582
mujeebalish <mujeebrehman12122@gmail.com>
2024-09-28 02:33:22 -0700

    onAuth chnged

--- main.js
+++ main.js
@@ -42,8 +42,14 @@ document.getElementById('adminBtn').addEventListener('click', function() {
         // onAut state change
         onAuthStateChanged(auth, (user) => {
           if (user) {
-            window.location.href = './dashbord/dashbord.html'
-          } 
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            const uid = user.uid;
+            // ...
+          } else {
+            // User is signed out
+            // ...
+          }
         });
       }); 
         
